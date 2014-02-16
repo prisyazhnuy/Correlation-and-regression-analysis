7b9dd6a53e20b70164362386a1910b9861d65f29
Maxim <prisyazhnuy@gmail.com>
2014-02-16 17:47:58 +0200

    Create mainwindow and add libraries

--- src/correlation/and/regression/analysis/CorrelationAndRegressionAnalysis.java
+++ src/correlation/and/regression/analysis/CorrelationAndRegressionAnalysis.java
@@ -3,7 +3,7 @@ package correlation.and.regression.analysis;
 public class CorrelationAndRegressionAnalysis {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        MainWindow.start();
     }
     
 }
